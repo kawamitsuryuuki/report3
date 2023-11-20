a66e36bed2c6eda5381c08cd5afd73fc3701dcf6
Kawamitu ryuuki <e235746@ie.u-ryukyu.ac.jp>
2023-11-20 12:48:36 +0900

    step1

--- app/src/main/java/jp/ac/uryukyu/ie/e235746/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235746/Enemy.java
@@ -67,11 +67,8 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        if (hitPoint > 0){
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
-        }
-        
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        hero.wounded(damage);    
     }
 
     /**
